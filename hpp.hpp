3ad53cc796bd829fe52e7ea93eeee573da4016ca
Dmitry Ponyatov <dponyatov@gmail.com>
2016-02-12 11:09:47 +0400

    * TOC(C,X)
    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- hpp.hpp
+++ hpp.hpp
@@ -33,7 +33,7 @@ struct Dir: Sim { Dir(string); };
 extern int yylex();
 extern int yylineno;
 extern char* yytext;
-#define TOC(X) { yylval.o = new Sim(yytext); return X; }
+#define TOC(C,X) { yylval.o = new C(yytext); return X; }
 extern int yyparse();
 extern void yyerror(string);
 #include "ypp.tab.hpp"
